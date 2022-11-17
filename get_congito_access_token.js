36a83586e08c66554c8c6e7cc5fec22ab957eda0
Mai <jea989@gmail.com>
2022-11-17 12:59:44 -0500

    first commit

---
+++ get_congito_access_token.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+const { UserPool } = require('cognito-srp');
+const axios = require('axios');
+const moment = require('moment-timezone');
+
+const main = async () => {
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
+  const clientId = process.env.CLIENTID;
+  const userPoolId = process.env.USERPOOLID;
+
+  if (!(process.env.USERNAME && process.env.PASSWORD && process.env.CLIENTID && process.env.USERPOOLID)) {
+    console.log('must provide USERNAME PASSWORD CLIENTID USERPOOLID as environment variables')
+    process.exit();
+  }
+
+  const [poolSrv, poolName] = userPoolId.split('_');
+  const userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0';
+
+  console.log(process.argv)
+  process.exit()
+  console.log('iiiiiiiiiii')
+
+  const userPool = new UserPool(poolName);
+  const challenge = await userPool.getClientChallenge({username, password});
+  const srpA = challenge.calculateA();
+
+  const firstResponse = await axios({
+    method: 'POST',
+    url: `https://cognito-idp.${poolSrv}.amazonaws.com/`,
+    headers: {
+      'Content-Type': 'application/x-amz-json-1.1',
+      'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
+      'User-Agent': userAgent,
+    },
+    data: {
+      'AuthFlow': 'USER_SRP_AUTH',
+      'ClientId': clientId,
+      'AuthParameters': {
+        'USERNAME': username,
+        'SRP_A': srpA.toString("hex"),
+      },
+      'ClientMetadata': {},
+    }
+  });
+
+  const timestamp = firstResponse.headers.date;
+  const fixedTimestamp = moment(timestamp).tz('UTC').format('ddd MMM DD HH:mm:ss zz YYYY');
+  const srpB = firstResponse.data.ChallengeParameters.SRP_B;
+  const salt = firstResponse.data.ChallengeParameters.SALT;
+  const secretBlock = firstResponse.data.ChallengeParameters.SECRET_BLOCK;
+  const session = challenge.getSession(srpB, salt);
+  const signature = session.calculateSignature(secretBlock, fixedTimestamp);
+
+  const secondResponse = await axios({
+    method: 'POST',
+    url: `https://cognito-idp.${poolSrv}.amazonaws.com/`,
+    headers: {
+      'Content-Type': 'application/x-amz-json-1.1',
+      'X-Amz-Target': 'AWSCognitoIdentityProviderService.RespondToAuthChallenge',
+      'User-Agent': userAgent
+    },
+    data: {
+      'ChallengeName': "PASSWORD_VERIFIER",
+      'ClientId': clientId,
+      'ChallengeResponses': {
+        'USERNAME': username,
+        'PASSWORD_CLAIM_SECRET_BLOCK': secretBlock,
+        'TIMESTAMP': fixedTimestamp, 
+        'PASSWORD_CLAIM_SIGNATURE': signature,
+      },
+      'ClientMetadata': {},
+    }
+  });
+
+  process.stdout.write(secondResponse.data.AuthenticationResult.AccessToken);
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,13 @@
+# get_congito_access_token
+
+perform the handshake to get your access token from aws cognito 
+
+how to use:
+
+```
+git clone https://github.com/mai-gh/get_congito_access_token
+cd get_congito_access_token
+npm install
+
+USERNAME=aaa PASSWORD=bbb CLIENTID=ccc USERPOOLID=ddd node get_congito_access_token.js
+```
